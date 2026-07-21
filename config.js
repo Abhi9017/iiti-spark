c5e872214067c4ccb5e7394d043a1acbc7de5577
Abhilash chinnala <101489733+Abhi9017@users.noreply.github.com>
2026-07-21 22:21:54 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,8 +3,8 @@
   Do not put a client secret in this file. Client IDs are public identifiers.
 */
 window.IITI_SPARK_CONFIG = {
-  API_URL: "PASTE_APPS_SCRIPT_WEB_APP_EXEC_URL_HERE",
-  GOOGLE_CLIENT_ID: "PASTE_GOOGLE_WEB_CLIENT_ID_HERE.apps.googleusercontent.com",
+  API_URL: "https://script.google.com/macros/s/AKfycbw-nQwXQkT_iBO3J1WCvMOAAfu7FWT6njmn3y6RGh_cK6t-SFt3v3mONuUKYXRWDN-RiA/exec",
+  GOOGLE_CLIENT_ID: "539621938666-klr1dsg3cfoq4p1f03dras4k33l6h3cg.apps.googleusercontent.com",
   ALLOWED_DOMAIN: "iiti.ac.in",
   APP_NAME: "IITI Spark",
   TERMS_VERSION: "2026-07-21",
